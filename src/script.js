f989c7bd6b2d9c4142bbf1f89f5f5abf779306a6
Dmitry <rekssobaka@gmail.com>
2023-05-02 21:10:00 +0300

    reworkhw03

--- src/script.js
+++ src/script.js
@@ -2,13 +2,14 @@
 
 let n = parseInt(prompt('Enter N number'));
 let count = 0;
-if (!isNaN(n) && n >= 0);
-else {
-    console.log('Enter correct number');
-}
-    for (let i = 1; i <= n; i++) {
+
+if (!isNaN(n)) {
+    for (let i = 1; i < n; i++) {
       if (i % 5 === 0) {
         count++;
       }
-    }console.log(`Numbers divisible by 5 and less than ${n}: ${count}`);
-    
+    }
+    console.log(`Numbers divisible by 5 and less than ${n}: ${count}`);
+} else {
+    console.log('Enter correct number');
+}
